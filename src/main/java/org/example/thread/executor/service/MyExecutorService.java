be015108c0b2f6622cae32289e59ee317c23996e
sharadpawar01 <sharadpawar01@gmail.com>
2023-03-12 18:39:38 +0530

    Executor service

--- src/main/java/org/example/thread/executor/service/MyExecutorService.java
+++ src/main/java/org/example/thread/executor/service/MyExecutorService.java
@@ -1,25 +1,46 @@
 package org.example.thread.executor.service;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class MyExecutorService {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ExecutorService exService = Executors.newFixedThreadPool(2);
         System.out.println(Thread.currentThread().getName());
 
-        Runnable sp = ()->{
-            for(int i=0;i<3;i++){
-                System.out.println( i+ " " +Thread.currentThread().getName());
+        List<Future<Integer>> futureList = new ArrayList<>();
+        for (int i=10;i<=50;i+=10){
+            Future<Integer> value = exService.submit(new CTR(i));
+            futureList.add(value);
+        }
 
+        futureList.stream().forEach(future-> {
+            try {
+                System.out.println("Element in future --> "+future.get());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
             }
-        };
-        exService.submit(sp);
-        exService.submit(sp);
+        });
 
-        System.out.println("Available Processors ---> "+Runtime.getRuntime().availableProcessors());
         exService.shutdown();
         System.out.println("Main method finished");
+    }
+}
+class CTR implements Callable<Integer>{
+
+    private int number;
+
+    public CTR(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return number * 2;
     }
 }
