05c657134ed75981043c1680448bb5af7e50d155
sharadpawar01 <sharadpawar01@gmail.com>
2023-08-12 18:14:14 +0530

    conditional breakpoint in git

---
+++ src/main/java/org/example/streams/ConditionalBreakpoint.java
@@ -0,0 +1,10 @@
+package org.example.streams;
+
+public class ConditionalBreakpoint {
+    public static void main(String[] args) {
+
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
